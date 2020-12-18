b95e7fbc010f236161e6c180a4e1def0e7544500
Tola Anjorin <tola.anjorin@gmail.com>
2020-12-18 03:02:26 -0500

    update helper for week9

--- week9/mongo_helper.js
+++ week9/mongo_helper.js
@@ -1,29 +1,25 @@
-
 const toSentenceCase = (str) => str.charAt(0).toUpperCase() + str.substr(1);
 
 /**
  * Translates a Mongoose ValidatorError or MongoDB DuplicateKeyError into a human readable string.
- * 
+ *
  * Example usage :
- * 
+ *
  *  import helper from 'mongo_helper';
- *  
- *  ...
  *  try {
  *   const doc = new Model({...})
- *   const savedDoc =  await doc.save()
+ *   return await doc.save()
  *  } catch (error) {
  *    // converts the error object to an array of error messages.
- *    const errors =  helper.translateError(error)
+ *    return helper.translateError(error)
  *  }
- * 
- * ...
- * 
- * @param {*} err 
+ * @param {*} err
  */
 const translateError = (err) => {
   const errors = [];
-  if (err.code === 11000) {
+  if (!err.code && !err.errors) {
+    errors.push(err.message);
+  } else if (err.code === 11000) {
     const field = Object.keys(err.keyPattern)[0];
     errors.push(toSentenceCase(`${field} already exists.`));
   } else {
